1dbdc0d891fcbd27679ba80d9c2fed15d6dcc5a2
harishVSP <136056568+harishVSP@users.noreply.github.com>
2024-06-10 08:00:55 +0530

    SoundBoard

---
+++ Soundboard.js
@@ -0,0 +1,21 @@
+const sounds = ["AllahuAkbar", "Deez", "Illuminati", "JustDoIt", "LeeroyJenkins","Wasted","wat"];
+const buttons = document.getElementById("buttons");
+
+const stopSounds = () => {
+  sounds.forEach((sound) => {
+    const currentSound = document.getElementById(sound);
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  });
+};
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+  buttons.appendChild(btn);
+});
